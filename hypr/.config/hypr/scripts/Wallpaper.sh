99282f3b187491a3848a76a66ae96ee28456aef0
Tom Robinson <github@mail.tomrobinson.io>
2026-02-20 17:07:00 -0500

    Modify for hyprpaper

--- hypr/.config/hypr/scripts/Wallpaper.sh
+++ hypr/.config/hypr/scripts/Wallpaper.sh
@@ -1,48 +1,6 @@
 #!/bin/bash
-
-DIR=$HOME/Pictures/wallpapers/
-PICS=($(find ${DIR} -type f \( -name "*.jpg" -o -name "*.jpeg" -o -name "*.png" -o -name "*.gif" \)))
-RANDOMPICS=${PICS[ $RANDOM % ${#PICS[@]} ]}
-
-change_swaybg(){
-  pkill swww
-  pkill swaybg
-  swaybg -m fill -i ${RANDOMPICS}
-}
-
-change_swww(){
-  pkill swaybg
-  swww query || swww init
-  swww img ${RANDOMPICS} --transition-fps 30 --transition-type any --transition-duration 3
-}
-
-change_current(){
-  if pidof swaybg >/dev/null; then
-    change_swaybg
-  else
-    change_swww
-  fi
-}
-
-switch(){
-  if pidof swaybg >/dev/null; then
-    change_swww
-  else
-    change_swaybg
-  fi
-}
-
-case "$1" in
-	"swaybg")
-		change_swaybg
-		;;
-	"swww")
-		change_swww
-		;;
-  "s")
-		switch
-		;;
-	*)
-		change_current
-		;;
-esac
+WALLPAPER_DIR="$HOME/Pictures/Wallpapers"
+WALLPAPER=$(ls "$WALLPAPER_DIR" | shuf -n 1)
+hyprctl hyprpaper wallpaper "DP-1,$WALLPAPER_DIR/$WALLPAPER"
+hyprctl hyprpaper wallpaper "DP-2,$WALLPAPER_DIR/$WALLPAPER"
+sleep 1
